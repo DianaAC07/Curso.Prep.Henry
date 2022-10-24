e681e4e81e1142bf77feb2f3b7e50ca4c5f0249d
Diana <dalcalac80@gmail.com>
2022-10-23 19:41:28 -0500

    resueltos hasta el 15

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,8 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  str===nuevaString;
+  return str;
   
 }
 
@@ -35,59 +37,91 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  x===5;
+  y===3;
+  r=x+y;
+  return r;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  x===5;
+  y===3;
+  r2=x-y;
+  return r2;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  x===5;
+  y===3;
+  r3=x*y;
+  return r3;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  x===5;
+  y===3;
+  r4=x/y;
+  return r4;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  x===5;
+  y===3;
+  if (x == y)
+    return true;
+  else
+    return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  str1==="Diana";
+  str2==="Castellanos";
+  if (str1.length == str2.length)
+    return true;
+  else
+    return false;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  num===0;
+  if (num < 90)
+    return true;
+  else
+    return false;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  num===0;
+  if (num > 50)
+    return true;
+  else
+    return false;
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  x===0;
+  y===0;
+  resto=x%y;
 }
 
 function esPar(num) {
